cf01fdc88d45372895e648b5032012eb124f14f1
Angela <avenable11@ivytech.edu>
2024-11-06 20:42:32 -0500

    this pointer

--- 11_06_2024/course.cpp
+++ 11_06_2024/course.cpp
@@ -1,1 +1,6 @@
 #include "course.h"
+
+course::course(deptType dept, int courseNum, std::string sectNum, const dayType days[], int numDays, int startHour, int startMin, int endHour, int endMin, std::string building, std::string campus, int roomNum)
+{
+    this->dept = dept;
+}
