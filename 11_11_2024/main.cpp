aec354220396b1933be9dd3352715f712df47c59
Angela Venable <avenable11@ivytech.edu>
2024-11-11 19:01:06 -0500

    polymorphism part 1

---
+++ 11_11_2024/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include "person.h"
+
+enum personType
+{
+    PERSON,
+    EMPLOYEE,
+    STUDENT
+};
+
+void addToParty(person **party, person *, int &nextPos);
+person createPerson();
+employee createEmployee();
+student *createStudent();
+
+int main()
+{
+    person **party;
+    party = new person *[10];
+    int position = 0;
+    while (true)
+    {
+        personType type;
+        int personInt;
+        std::cout << "What kind of person will be attending the party?" << std::endl;
+        std::cout << "1. Person" << std::endl;
+        std::cout << "2. Employee" << std::endl;
+        std::cout << "3. Student" << std::endl;
+        std::cin >> personInt;
+        personInt -= 1;
+
+        // insert validation loop here
+        switch (personInt)
+        {
+        case PERSON:
+        {
+            person p = createPerson();
+            addToParty(party, &p, position);
+            break;
+        }
+        case EMPLOYEE:
+        {
+            employee e = createEmployee();
+            addToParty(party, &e, position);
+            break;
+        }
+        case STUDENT:
+            break;
+            // p = createStudent();
+        }
+        // addToParty(party, p, position);
+        if (position == 10)
+        {
+            break;
+        }
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << party[i]->tostring() << std::endl
+                  << std::endl;
+    }
+    return 0;
+}
+
+void addToParty(person **party, person *personToAdd, int &position)
+{
+    party[position] = personToAdd->makeCopy();
+    position++;
+}
+
+person createPerson()
+{
+
+    return person("Harry", "Potter", 40, WHITE, 5.7, 175, "brown", "green");
+}
+
+employee createEmployee()
+{
+
+    return employee("Santa", "Claus", 565, OTHER, 6, 400, "white", "blue", "2024-11-10", 60000, 1, "Head delivery guy", false);
+}
+
+student *createStudent()
+{
+    return nullptr;
+}
---
+++ 11_11_2024/person.h
@@ -0,0 +1,106 @@
+#ifndef PERSON_H
+#define PERSON_H
+#include <string>
+enum raceType
+{
+    NATIVE,
+    ISLANDER,
+    HISPANIC,
+    WHITE,
+    ASIAN,
+    BLACK,
+    OTHER
+};
+const int NUM_RACES = 7;
+const raceType races[] = {NATIVE,
+                          ISLANDER,
+                          HISPANIC,
+                          WHITE,
+                          ASIAN,
+                          BLACK,
+                          OTHER};
+const std::string raceToStr[] = {"Native American",
+                                 "Pacific Islander",
+                                 "Hispanic",
+                                 "White",
+                                 "Asian",
+                                 "Black",
+                                 "Other"};
+class person
+{
+public:
+    person(std::string first_name, std::string last_name, int a, raceType r, double h, double w, std::string hair, std::string eye, char middle = '\0');
+    std::string getName();
+    std::string getFName();
+    std::string getLName();
+    std::string getMI();
+    int getAge();
+    std::string getRace();
+    double getHeight();
+    double getWeight();
+    std::string getHairColor();
+    std::string getEyeColor();
+    void setName(std::string fn, std::string ln, char m = '\0');
+    void setFName(std::string);
+    void setLName(std::string);
+    void setMI(char);
+    void incrementAge(int);
+    void setHeight(double);
+    void setWeight(double);
+    void setHairColor(std::string);
+    virtual std::string tostring();
+    virtual person *makeCopy();
+
+protected:
+    std::string fname;
+    std::string lname;
+    char mi;
+    int age;
+    raceType race;
+    double height;
+    double weight;
+    std::string hairColor;
+    std::string eyeColor;
+};
+
+class employee : public person
+{
+public:
+    employee(std::string first_name, std::string last_name, int a, raceType r, double h, double w, std::string hair, std::string eye, std::string hire, double sal, unsigned int id, std::string pos, bool hour, char middle = '\0');
+    std::string getHireDate();
+    double getSalary();
+    int getEmpID();
+    std::string getPosition();
+    bool isHourly();
+    void setSalary(double salary);
+    void setPosition(std::string pos);
+    void setHourly(bool);
+    std::string tostring();
+    employee *makeCopy();
+
+private:
+    std::string hireDate;
+    double salary;
+    unsigned int empID;
+    std::string position;
+    bool hourly;
+};
+
+class course
+{
+    std::string dept;
+    int courseNum;
+    std::string section;
+    int semester;
+    int creditHours;
+    char grade;
+};
+
+class student : public person
+{
+    double GPA;
+    std::string major;
+    course schedule;
+};
+
+#endif
